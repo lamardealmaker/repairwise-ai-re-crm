38adcb5241790585fc11ecae9b1fd084241eff5a
lamardealmaker <41444155+lamardealmaker@users.noreply.github.com>
2025-01-26 09:08:43 -0600

    👍 IMPROVE: improved landing page back to prev design

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -6,6 +6,7 @@ This server layout provides a shared header and basic structure for (marketing) 
 
 "use server"
 
+import Header from "@/components/header"
 export default async function MarketingLayout({
   children
 }: {
@@ -13,6 +14,8 @@ export default async function MarketingLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col" data-oid="xjeoin2">
+      <Header data-oid="0v7v-5t" />
+
       <div className="flex-1" data-oid="1f9z6fu">
         {children}
       </div>
